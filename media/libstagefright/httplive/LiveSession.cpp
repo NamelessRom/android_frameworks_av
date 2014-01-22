3f68d7450ee39372c19d3c4d8ff42c485cd0e930
ywan171 <yi.a.wang@intel.com>
2014-01-22 16:28:32 +0800

    LiveSession: check bandwidth in 10s interval to support adaptive stream switch
    schedule another check bandwidth event when previous bandwidth check event responds.
    so that HLS will realize adaptive stream switch when network bandwidh change.
    
    the bug exists in kitkat 4.4.2, brough by AOSP upgrade and it will impact HLS
    bandwidth switch feature
    
    Steps to reproduce:
     1. connect phone /tablet to wifi, adjust router to set initial wifi bandwidth to low bandwidth
     2. lauch browser to access http live streaming server and start playback on full screen (there're three TS streaming for 900kbps/300kbps/100kbps bandwidth)
     3. During playing streaming, change network bandwidth to be high,such as 1000kbps
    
    expected result: streaming switches to high quality
    actual result: streaming does not switch to high quality
    
    Change-Id: Ib662952ee0fa1574d17d81a3e15cbbc74b47ff1d
    Signed-off-by: ywan171 <yi.a.wang@intel.com>

--- media/libstagefright/httplive/LiveSession.cpp
+++ media/libstagefright/httplive/LiveSession.cpp
@@ -1258,6 +1258,7 @@ void LiveSession::onChangeConfiguration3(const sp<AMessage> &msg) {
     // All fetchers have now been started, the configuration change
     // has completed.
 
+    mCheckBandwidthGeneration++;
     scheduleCheckBandwidthEvent();
 
     ALOGV("XXX configuration change completed.");
@@ -1375,6 +1376,7 @@ void LiveSession::onCheckBandwidth() {
     // This ensures that only one configuration change is ongoing at any
     // one time, once that completes it'll schedule another check bandwidth
     // event.
+    scheduleCheckBandwidthEvent();
 }
 
 void LiveSession::postPrepared(status_t err) {
