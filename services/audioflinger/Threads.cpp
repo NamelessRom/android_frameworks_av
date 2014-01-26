e3325146b30784e95e0206016c20d8ca99c0776d
Alexander Martinz <eviscerationls@gmail.com>
2014-01-26 14:28:59 +0100

    Introduce AUDIO_LEGACY_HACK to extend support for legacy devices
    this is a dirty hack but does what it should.
    
    if we build for legacy device, eg LG Optimus Black P970, we have to use
    the ICS_AUDIO_BLOBS flag to get audio.
    
    but if we add it, we get build breakages and if we remove it, we dont have
    audio at all.
    
    this hack allows us to set the AUDIO_LEGACY_HACK flag in the BoardConfig and
    prevents build breakages.
    
    as i am unsure if every device needs that, i couldnt wrap it around the
    ICS_AUDIO_BLOBS flag, thus this new flag is added.
    
    Change-Id: Id545a684fcdc0508f4135eb7a722a9b617595532

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -1740,6 +1740,7 @@ void AudioFlinger::PlaybackThread::readOutputParameters()
                 mFrameCount);
     }
 
+#ifndef AUDIO_LEGACY_HACK
     if ((mOutput->flags & AUDIO_OUTPUT_FLAG_NON_BLOCKING) &&
             (mOutput->stream->set_callback != NULL)) {
         if (mOutput->stream->set_callback(mOutput->stream,
@@ -1748,6 +1749,7 @@ void AudioFlinger::PlaybackThread::readOutputParameters()
             mCallbackThread = new AudioFlinger::AsyncCallbackThread(this);
         }
     }
+#endif
 
     // Calculate size of normal mix buffer relative to the HAL output buffer size
     double multiplier = 1.0;
@@ -2036,6 +2038,7 @@ ssize_t AudioFlinger::PlaybackThread::threadLoop_write()
 
 void AudioFlinger::PlaybackThread::threadLoop_drain()
 {
+#ifndef AUDIO_LEGACY_HACK
     if (mOutput->stream->drain) {
         ALOGD("copl(%d):draining %s", mId,
             (mMixerStatus == MIXER_DRAIN_TRACK) ? "early" : "full");
@@ -2049,6 +2052,7 @@ void AudioFlinger::PlaybackThread::threadLoop_drain()
             (mMixerStatus == MIXER_DRAIN_TRACK) ? AUDIO_DRAIN_EARLY_NOTIFY
                                                 : AUDIO_DRAIN_ALL);
     }
+#endif
 }
 
 void AudioFlinger::PlaybackThread::threadLoop_exit()
@@ -2571,6 +2575,7 @@ status_t AudioFlinger::PlaybackThread::getTimestamp_l(AudioTimestamp& timestamp)
     if (mNormalSink != 0) {
         return mNormalSink->getTimestamp(timestamp);
     }
+#ifndef AUDIO_LEGACY_HACK
     if (mType == OFFLOAD && mOutput->stream->get_presentation_position) {
         uint64_t position64;
         int ret = mOutput->stream->get_presentation_position(
@@ -2580,6 +2585,7 @@ status_t AudioFlinger::PlaybackThread::getTimestamp_l(AudioTimestamp& timestamp)
             return NO_ERROR;
         }
     }
+#endif
     return INVALID_OPERATION;
 }
 // ----------------------------------------------------------------------------
@@ -4105,13 +4111,17 @@ AudioFlinger::OffloadThread::OffloadThread(const sp<AudioFlinger>& audioFlinger,
 
 void AudioFlinger::OffloadThread::threadLoop_exit()
 {
+#ifndef AUDIO_LEGACY_HACK
     if (mFlushPending || mHwPaused) {
         // If a flush is pending or track was paused, just discard buffered data
         flushHw_l();
     } else {
+#endif
         mMixerStatus = MIXER_DRAIN_ALL;
         threadLoop_drain();
+#ifndef AUDIO_LEGACY_HACK
     }
+#endif
     mCallbackThread->exit();
     PlaybackThread::threadLoop_exit();
 }
@@ -4286,6 +4296,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::OffloadThread::prepareTr
     // If a flush is pending and a track is active but the HW is not paused, force a HW pause
     // before flush and then resume HW. This can happen in case of pause/flush/resume
     // if resume is received before pause is executed.
+#ifndef AUDIO_LEGACY_HACK
     if (!mStandby && (doHwPause || (mFlushPending && !mHwPaused && (count != 0)))) {
         ALOGD("copl(%d):pause hal", mId);
         mOutput->stream->pause(mOutput->stream);
@@ -4301,6 +4312,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::OffloadThread::prepareTr
         ALOGD("copl(%d):resume hal", mId);
         mOutput->stream->resume(mOutput->stream);
     }
+#endif
 
     // remove all the tracks that need to be...
     removeTracks_l(*tracksToRemove);
@@ -4347,6 +4359,7 @@ bool AudioFlinger::OffloadThread::waitingAsyncCallback()
 
 void AudioFlinger::OffloadThread::flushHw_l()
 {
+#ifndef AUDIO_LEGACY_HACK
     ALOGD("copl(%d): flush hal", mId);
     mOutput->stream->flush(mOutput->stream);
     // Flush anything still waiting in the mixbuffer
@@ -4365,6 +4378,7 @@ void AudioFlinger::OffloadThread::flushHw_l()
         mCallbackThread->setWriteBlocked(mWriteAckSequence);
         mCallbackThread->setDraining(mDrainSequence);
     }
+#endif
 }
 
 // ----------------------------------------------------------------------------
