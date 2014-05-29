d8722a5e7fa59137dc42737c83aaf8ac087cc613
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-05-29 09:28:58 +0200

    Revert "stagefright: include nAllocLen parameter with OMX Buffer"
    This reverts commit a8a97f3a0642e855ddbacda6c4a7243730fe2784.
    
    Unlike on Gingerbread, we are using Metadata to allocate Gralloc Buffers with ION. Hence there is no need to explicitly override the nAllocLen parameter since we are dealing with Gralloc Pointers and not actual data.
    
    Change-Id: I0d86f3933e652c21af41f08a19d0e14bced90fbe

--- include/media/stagefright/OMXCodec.h
+++ include/media/stagefright/OMXCodec.h
@@ -183,8 +183,6 @@ private:
         size_t mSize;
         void *mData;
         MediaBuffer *mMediaBuffer;
-        OMX_U8 *mAllocatedBuffer;
-        OMX_U32 mAllocatedSize;
         bool mOutputCropChanged;
     };
 
