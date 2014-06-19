9533d786848aaaacecb8d4d0a6b47301f9eea132
Haynes Mathew George <hgeorge@codeaurora.org>
2014-06-19 11:53:11 -0700

    frameworks/av: miscellaneous fixes
    fixes for the following issues reported by KW
    
    5978, 8879, 8880, 8881, 8882, 8883, 8889, 8890,
    8892, 8893, 9200, 9201, 10210, 11223, 11224, 18001,
    83677, 84525, 5897
    
    Change-Id: I9bb98a07af97cbfc746b21738dfce7cd9e128c94

--- media/libstagefright/ExtendedUtils.cpp
+++ media/libstagefright/ExtendedUtils.cpp
@@ -611,6 +611,10 @@ int32_t ExtendedUtils::getEncoderTypeFlags() {
 
 void ExtendedUtils::prefetchSecurePool(const char *uri)
 {
+    if (!uri) {
+        return;
+    }
+
     if (!strncasecmp("widevine://", uri, 11)) {
         ALOGV("Widevine streaming content\n");
         createSecurePool();
