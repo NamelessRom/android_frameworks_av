d037a1035b7c74cef23522bac3f5d2051819934f
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-06-23 12:15:40 +0530

    libstagefright: update OMAP3 support
    * Move the OMAP_ENHANCENEMENT flag to make sure that the
      buffer is not modified futher on.
    * Don't queue more than 4 buffers to the component. This
      greatly improved the video recording framerates both for
      480P and 720P.
    
    Change-Id: Iacc983214d873099e8f6218f374717a2ae4ffcc8

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -3598,6 +3598,12 @@ void OMXCodec::drainInputBuffers() {
                 continue;
             }
 
+#if defined (OMAP_ENHANCEMENT) && defined (TARGET_OMAP3)
+            if (mIsEncoder && mIsVideo && (i == 4)) {
+                break;
+            }
+#endif
+
             if (!drainInputBuffer(info)) {
                 break;
             }
@@ -3786,23 +3792,6 @@ bool OMXCodec::drainInputBuffer(BufferInfo *info) {
         }
 
         bool releaseBuffer = true;
-#ifdef OMAP_ENHANCEMENT
-        if (mIsEncoder && (mQuirks & kAvoidMemcopyInputRecordingFrames)) {
-            CHECK(mOMXLivesLocally && offset == 0);
-
-            OMX_BUFFERHEADERTYPE *header =
-                (OMX_BUFFERHEADERTYPE *)info->mBuffer;
-
-            CHECK(header->pBuffer == info->mData);
-
-            header->pBuffer =
-                (OMX_U8 *)srcBuffer->data() + srcBuffer->range_offset();
-
-            releaseBuffer = false;
-            info->mMediaBuffer = srcBuffer;
-        } else {
-#endif
-
         if (mFlags & kStoreMetaDataInVideoBuffers) {
                 releaseBuffer = false;
                 info->mMediaBuffer = srcBuffer;
@@ -3815,6 +3804,21 @@ bool OMXCodec::drainInputBuffer(BufferInfo *info) {
 
                 CHECK(info->mMediaBuffer == NULL);
                 info->mMediaBuffer = srcBuffer;
+#ifdef OMAP_ENHANCEMENT
+        } else if (mIsEncoder && (mQuirks & kAvoidMemcopyInputRecordingFrames)) {
+                CHECK(mOMXLivesLocally && offset == 0);
+
+                OMX_BUFFERHEADERTYPE *header =
+                    (OMX_BUFFERHEADERTYPE *)info->mBuffer;
+
+                CHECK(header->pBuffer == info->mData);
+
+                header->pBuffer =
+                    (OMX_U8 *)srcBuffer->data() + srcBuffer->range_offset();
+
+                releaseBuffer = false;
+                info->mMediaBuffer = srcBuffer;
+#endif
         } else {
 #ifdef USE_SAMSUNG_COLORFORMAT
             OMX_PARAM_PORTDEFINITIONTYPE def;
@@ -3858,10 +3862,6 @@ bool OMXCodec::drainInputBuffer(BufferInfo *info) {
                     srcBuffer->range_length());
 #endif // USE_SAMSUNG_COLORFORMAT
         }
-
-#ifdef OMAP_ENHANCEMENT
-	}
-#endif
 
         int64_t lastBufferTimeUs;
         CHECK(srcBuffer->meta_data()->findInt64(kKeyTime, &lastBufferTimeUs));
