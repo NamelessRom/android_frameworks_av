1304509aea2ed18017bd9afa922528c217156d37
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-09-01 06:20:55 +0530

    libstagefright: fix drainInputBuffers for FlushCompleteEmulation Quirk
    Codecs which use the "requires-flush-complete-emulation" require
    flushing (draining) the buffers before filling them. The change
    "accelerate camera recording start up speed" has changed the order
    of filling and draining the buffers which doesn't work well with
    the FlushCompleteEmulation quirk
    
    Change-Id: I78193f0aca4a85455393a4cf20d33b919ea6a57e

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -5130,13 +5130,16 @@ status_t OMXCodec::read(
             mPaused = false;
         }
 
+        if (mQuirks & kRequiresFlushCompleteEmulation)
+            drainInputBuffers();
 
         if (mState == EXECUTING) {
             // Otherwise mState == RECONFIGURING and this code will trigger
             // after the output port is reenabled.
             fillOutputBuffers();
         }
-        drainInputBuffers();
+        if (!(mQuirks & kRequiresFlushCompleteEmulation))
+            drainInputBuffers();
     }
 
     if (seeking) {
