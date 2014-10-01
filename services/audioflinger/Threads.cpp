b59d504773d91bf803d78aa94e3451b29460ba4c
Tom Marshall <tdm@cyngn.com>
2014-10-01 15:45:08 -0700

    av: audioflinger: Fix SRS build
    Change-Id: I1c8bbf62e2a96ce7ccf303427d18a542dbbc0af2

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -2296,12 +2296,12 @@ bool AudioFlinger::PlaybackThread::threadLoop()
     POSTPRO_PATCH_PARAMS_SET(bt_param);
     if (mType == MIXER) {
         POSTPRO_PATCH_OUTPROC_PLAY_INIT(this, myName);
-    } else if (mType == DUPLICATING) {
-        POSTPRO_PATCH_OUTPROC_DUPE_INIT(this, myName);
     } else if (mType == OFFLOAD) {
         POSTPRO_PATCH_OUTPROC_DIRECT_INIT(this, myName);
+        POSTPRO_PATCH_OUTPROC_PLAY_ROUTE_BY_VALUE(this, mOutDevice);
     } else if (mType == DIRECT) {
         POSTPRO_PATCH_OUTPROC_DIRECT_INIT(this, myName);
+        POSTPRO_PATCH_OUTPROC_PLAY_ROUTE_BY_VALUE(this, mOutDevice);
     }
 #endif
 
@@ -2480,8 +2480,6 @@ bool AudioFlinger::PlaybackThread::threadLoop()
 #ifdef SRS_PROCESSING
                 if (mType == MIXER && mMixerStatus == MIXER_TRACKS_READY) {
                     POSTPRO_PATCH_OUTPROC_PLAY_SAMPLES(this, mFormat, mMixBuffer, mixBufferSize, mSampleRate, mChannelCount);
-                } else if (mType == DUPLICATING) {
-                    POSTPRO_PATCH_OUTPROC_DUPE_SAMPLES(this, mFormat, mMixBuffer, mixBufferSize, mSampleRate, mChannelCount);
                 } /* else if (mType == OFFLOAD) {
                     POSTPRO_PATCH_OUTPROC_DIRECT_SAMPLES(this, mFormat, mMixBuffer, mixBufferSize, mSampleRate, mChannelCount);
                 } else if (mType == DIRECT) {
@@ -2556,8 +2554,6 @@ if (mType == MIXER) {
 #ifdef SRS_PROCESSING
     if (mType == MIXER) {
         POSTPRO_PATCH_OUTPROC_PLAY_EXIT(this, myName);
-    } else if (mType == DUPLICATING) {
-        POSTPRO_PATCH_OUTPROC_DUPE_EXIT(this, myName);
     } else if (mType == OFFLOAD) {
         POSTPRO_PATCH_OUTPROC_DIRECT_EXIT(this, myName);
     } else if (mType == DIRECT) {
