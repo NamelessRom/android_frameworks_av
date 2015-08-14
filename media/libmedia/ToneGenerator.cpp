1ee9fb9698894cb00d8da3531e735a4cd0cb7a3c
Mykola Kondratenko <mykola.kondratenko@sonymobile.com>
2015-08-14 15:01:17 +0200

    Fix WaveGenerator memory leak
    Releasing memory with ToneGenerator::stopTone() depends on
    ToneGenerator::mState that can cause memory leak in case
    mState is TONE_INIT.
    
    Remove dependency to mState by adding ToneGenerator::clearWaveGens()
    explicit call in ToneGenerator destructor.
    
    Change-Id: I5ccfbca9f2ae1abac9e160a2a309fb6962d4a110

--- media/libmedia/ToneGenerator.cpp
+++ media/libmedia/ToneGenerator.cpp
@@ -865,6 +865,7 @@ ToneGenerator::~ToneGenerator() {
         ALOGV("Delete Track: %p", mpAudioTrack.get());
         mpAudioTrack.clear();
     }
+    clearWaveGens();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
